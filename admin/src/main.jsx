7fee5dfa9704a2dfe237ceb238f9051ce0b84fe9
alik3002 <95424211+alik3002@users.noreply.github.com>
2025-12-13 12:52:05 +0600

    feat: restructure styles and add new CSS files for improved layout and design

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// Reuse existing styles from main site
-import '../../src/index.css'
-import '../../src/App.css'
+import './index.css'
+import './admin.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
